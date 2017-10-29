6d020ce45e2ae22326aa3a0ea2f8caf8838aa3ca
zhoushaodong <1138894663@qq.com>
2017-10-29 22:36:21 +0800

    777

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/service/GPSLocationService.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/service/GPSLocationService.java
@@ -1,10 +1,8 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard.service;
 
 
-
 import android.Manifest;
 import android.app.Service;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -14,16 +12,12 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
-
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.telephony.SmsManager;
 
-
-
-
-
 /**
- * Created by 11388 on 2017/10/29.
+ * Created by hh on 2017/10/29.
  */
 
 public class GPSLocationService extends Service {
@@ -34,46 +28,39 @@ public class GPSLocationService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
-
     @Override
-    public void onCreate() {
+    public void onCreate(){
         super.onCreate();
-        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        lm = (LocationManager)getSystemService(LOCATION_SERVICE);
         listener = new MyListener();
 
         Criteria criteria = new Criteria();
         criteria.setAccuracy(Criteria.ACCURACY_FINE);
         criteria.setCostAllowed(true);
         String name = lm.getBestProvider(criteria, true);
-
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+                && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED){
             return;
         }
-        lm.requestLocationUpdates(name, 0, 0, listener);
+        lm.requestLocationUpdates(name,0,0,listener);
     }
-
-
-
-
     private class MyListener implements LocationListener {
-
 
         @Override
         public void onLocationChanged(Location location) {
             StringBuilder sb = new StringBuilder();
             sb.append("accuracy:" + location.getAccuracy() + "\n");
             sb.append("speed:" + location.getSpeed() + "\n");
-            sb.append("Longitude:" + location.getLongitude() + "\n");
+            sb.append("Logitude:" + location.getLongitude() + "\n");
             sb.append("Latitude:" + location.getLatitude() + "\n");
             String result = sb.toString();
-            SharedPreferences sp = getSharedPreferences("config", MODE_PRIVATE);
+            SharedPreferences sp = getSharedPreferences("config", MODE_APPEND);
             String safenumber = sp.getString("safephone", "");
-
             SmsManager.getDefault().sendTextMessage(safenumber, null, result, null, null);
             stopSelf();
+
         }
 
         @Override
@@ -91,11 +78,10 @@ public class GPSLocationService extends Service {
 
         }
     }
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-            lm.removeUpdates(listener);
-            listener = null;
-        }
-
+    @Override
+    public void  onDestroy(){
+        super.onDestroy();
+        lm.removeUpdates(listener);
+        listener=null;
     }
+}
