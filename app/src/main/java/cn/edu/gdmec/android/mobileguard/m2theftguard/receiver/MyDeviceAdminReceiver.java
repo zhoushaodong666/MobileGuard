9715716802ed737960cdf692344279e730696f79
zhoushaodong <1138894663@qq.com>
2017-10-29 20:54:34 +0800

    111

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/MyDeviceAdminReceiver.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by 11388 on 2017/10/29.
+ */
+
+public class MyDeviceAdminReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
