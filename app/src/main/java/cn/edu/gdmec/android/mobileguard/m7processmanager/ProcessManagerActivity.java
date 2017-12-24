1f04985812ef239e31ce56a3cdd1e75a1ef0ebbc
zhoushaodong <1138894663@qq.com>
2017-12-24 14:36:57 +0800

    m7

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m7processmanager/ProcessManagerActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdmec.android.mobileguard.m7processmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
+public class ProcessManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process_manager);
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m7processmanager/entity/TaskInfo.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.android.mobileguard.m7processmanager.entity;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by 11388 on 2017/12/24.
+ */
+
+public class TaskInfo {
+    public String appName;
+    public Drawable appIcon;
+    public String packageName;
+    public long appMemory;
+    public boolean isChecked;
+    public boolean isUserApp;
+}
