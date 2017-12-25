b55ff0dbd1c887c5f95da39405a8ff1da2ea32a5
zhoushaodong <1138894663@qq.com>
2017-12-25 19:24:27 +0800

    小修改

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/db/dao/NumBelongtoDao.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/db/dao/NumBelongtoDao.java
@@ -5,12 +5,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.text.TextUtils;
 
-/**
- * Created by 11388 on 2017/12/24.
- */
+
 
 public class NumBelongtoDao {
-    public static String getLocation(Context context, String phonenumber){
+    public static String getLocation(Context context,String phonenumber){
         String location = phonenumber;
         String dbname = context.getFilesDir() + "/address.db";
         //System.out.println(dbname);
